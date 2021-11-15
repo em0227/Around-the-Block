ccfd6274f943e40a81c4775d209eb1b1af67f2a1
Snigdha  Banda <snigdhabanda@Snigdhas-MacBook-Air.local>
2021-11-15 14:54:45 -0500

    finished backend auth

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ mongoose
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 // app.get("/", (req, res) => res.send("Hello World"));
 app.use("/api/users", users);
 const port = process.env.PORT || 5000;
 
-app.use(passport.initialize());
-require('./config/passport')(passport);
+
 
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));

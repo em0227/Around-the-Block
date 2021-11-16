f21f2c87399df4a9830452d51e1ddd6abbdeb2bf
hien nguyen <thuhiennguyen198@gmail.com>
2021-11-16 06:27:45 -0500

    in the middle of styling forms

--- frontend/src/components/session/signup.jsx
+++ frontend/src/components/session/signup.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import { FaUser } from "react-icons/fa";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { GrLinkNext } from "react-icons/gr";
 
 class SignupForm extends React.Component {
   constructor(props) {
@@ -55,44 +58,73 @@ class SignupForm extends React.Component {
 
   render() {
     return (
-      <div className="signup-form-container">
-        <form onSubmit={this.handleSubmit}>
-          <div className="signup-form">
-            <br />
-            <input
-              type="text"
-              name="email"
-              value={this.state.email}
-              onChange={this.update("email")}
-              placeholder="Email"
-            />
-            <br />
-            <input
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={this.update("name")}
-              placeholder="Name"
-            />
-            <br />
-            <input
-              type="password"
-              value={this.state.password}
-              onChange={this.update("password")}
-              placeholder="Password"
-            />
-            <br />
-            <input
-              type="password"
-              value={this.state.confirmPassword}
-              onChange={this.update("confirmPassword")}
-              placeholder="Confirm Password"
-            />
-            <br />
-            <input type="submit" value="Submit" />
-            {this.renderErrors()}
+      <div className="form-container">
+        <div className="screen">
+          <div className="screen__content">
+            <form className="signup" onSubmit={this.handleSubmit}>
+              <div className="signup__field">
+                <br />
+                <input
+                  type="text"
+                  name="name"
+                  value={this.state.name}
+                  onChange={this.update("name")}
+                  placeholder="Name"
+                />
+              </div>
+              <br />
+              <div className="signup__field">
+                <i>
+                  <FaUser />
+                </i>
+                <input
+                  type="text"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.update("email")}
+                  placeholder="Email"
+                />
+              </div>
+              <br />
+              <div className="signup__field">
+                <i className="signup__icon fas fa-lock">
+                  <RiLockPasswordFill />
+                </i>
+                <input
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.update("password")}
+                  placeholder="Password"
+                />
+              </div>
+              <br />
+              <div className="signup__field">
+                <input
+                  type="password"
+                  value={this.state.confirmPassword}
+                  onChange={this.update("confirmPassword")}
+                  placeholder="Confirm Password"
+                />
+              </div>
+              <br />
+              <div>
+                <button className="button signup__submit" type="submit">
+                  <span className="button__text">LOG IN NOW</span>
+                  <i>
+                    <GrLinkNext />
+                  </i>
+                </button>
+                {this.renderErrors()}
+              </div>
+              <div class="screen__background">
+                <span class="screen__background__shape screen__background__shape4"></span>
+                <span class="screen__background__shape screen__background__shape3"></span>
+                <span class="screen__background__shape screen__background__shape2"></span>
+                <span class="screen__background__shape screen__background__shape1"></span>
+              </div>
+            </form>
           </div>
-        </form>
+        </div>
       </div>
     );
   }

c7def24d2944977cf923b3ee9935c24ae9cc70ba
Emily Wu <emilywu0227@gmail.com>
2021-11-16 23:33:18 -0500

    hide the keys

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,3 @@
-// module.exports = {
-//   mongoURI:
-//     "mongodb+srv://dev:5KwfzudwABxeN5MB@dev.shfnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-//   secretOrKey: "6KqCpOcPnX",
-//   //Make sure this is your own unique string
-// };
 if (process.env.NODE_ENV === "production") {
   module.exports = require("./keys_prod");
 } else {

7d924f73d818bfd29a6589c60309cc22d2e7cfe4
Emily Wu <emilywu0227@gmail.com>
2021-11-16 21:48:50 -0500

    refactor the url

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ require("./config/passport")(passport);
 
 // app.get("/", (req, res) => res.send("Hello World"));
 app.use("/api/users", users);
-app.use("/api/friendrequests", friendrequests);
+app.use("/api/friend-requests", friendrequests);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));

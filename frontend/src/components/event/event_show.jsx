f0e5df718f0246cb6f4c11085e29a8601f901ce2
Emily Wu <emilywu0227@gmail.com>
2021-11-17 17:29:34 -0500

    fixed typo

--- frontend/src/components/event/event_show.jsx
+++ frontend/src/components/event/event_show.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class EventShow extends React.Component {
   constructor(props) {
@@ -14,19 +14,24 @@ class EventShow extends React.Component {
     // const {event} = this.props.event
     return (
       <div className="event-show-page">
-                <div className="event-show-content">  
-                    <img className="show-img" src="https://atb-photos.s3.amazonaws.com/profile1.png" /> 
-                    <div clasName="event-show-detials">
-                        <div class="event-details">
-                            {/* <p>{event.time}</p> */}
-                            <p>6pm Thur. 11/18/2021</p>
-                            <br />
-                        </div>
-                        <div className="event-title">Cooking Party with EVEN!</div>
-                        <div className="description">We are going to cook with GAS!</div>
-                        <Link className="join-button" to="/signup">JOIN!</Link>
-                        </div>
-                </div>
+        <div className="event-show-content">
+          <img
+            className="show-img"
+            src="https://atb-photos.s3.amazonaws.com/profile1.png"
+          />
+          <div clasName="event-show-detials">
+            <div className="event-details">
+              {/* <p>{event.time}</p> */}
+              <p>6pm Thur. 11/18/2021</p>
+              <br />
+            </div>
+            <div className="event-title">Cooking Party with EVEN!</div>
+            <div className="description">We are going to cook with GAS!</div>
+            <Link className="join-button" to="/signup">
+              JOIN!
+            </Link>
+          </div>
+        </div>
       </div>
     );
   }

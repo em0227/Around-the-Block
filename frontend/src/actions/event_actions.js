214fc2287c4e1b150488bd0f35f9a87e0abcb15d
Snigdha  Banda <snigdhabanda@Snigdhas-MacBook-Air.local>
2021-11-16 20:18:30 -0500

    editing event_actions

---
+++ frontend/src/actions/event_actions.js
@@ -0,0 +1,59 @@
+import * as EventAPIUtil from "../util/session_api_util";
+
+export const RECEIVE_EVENT = "RECEIVE_EVENT"; 
+export const RECEIVE_EVENTS = "RECEIVE_EVENTS"; 
+export const REMOVE_EVENT = "REMOVE_EVENT"; 
+export const RECEIVE_EVENT_ERRORS = "RECEIVE_EVENT_ERRORS";
+
+const receiveEvent = (event) => ({
+    type: RECEIVE_EVENT,
+    event
+})
+
+const receiveEvents = (events) => ({
+    type: RECEIVE_EVENTS,
+    events
+})
+
+const removeEvent = () => ({
+    type: REMOVE_EVENT
+})
+
+const receiveEventErrors = (errors) => ({
+    type: RECEIVE_EVENT_ERRORS
+})
+
+export const createEvent = (event) => dispatch => (
+    EventAPIUtil.createEvent(event).then(
+        (event) => dispatch(receiveEvent(event.response.data)),
+        (err) => dispatch(receiveEventErrors(err.response.data))
+    )
+)
+
+export const updateEvent = (event) => dispatch => (
+    EventAPIUtil.updateEvent(event).then(
+        (event) => dispatch(receiveEvent(event.response.data)),
+        (err) => dispatch(receiveEventErrors(err.response.data))
+    )
+)
+
+export const fetchEvent = (eventId) => dispatch => (
+    EventAPIUtil.fetchEvent(eventId).then(
+        (event) => dispatch(receiveEvent(event.response.data)),
+        (err) => dispatch(receiveErrors(err.response.data))
+    )
+)
+
+export const fetchEvents = () => dispatch => (
+    EventAPIUtil.fetchEvents().then(
+        (events) => dispatch(receiveEvents(events)),
+        (err) => dispatch(receiveErrors(err.response.data))
+    )
+)
+
+export const deleteEvent = (eventId) => dispatch => (
+    EventAPIUtil.deleteEvent(eventId).then(
+        () => dispatch(removeEvent()),
+        (err) => dispatch(receiveErrors(err.response.data))
+    )
+)
---
+++ frontend/src/util/event_api_util.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const createEvent = (event) => {
+    return axios.post("/api/events/newEvent", event)
+}
+
+export const updateEvent = (event) => {
+    return axios.patch(`/api/events/${event.id}`, event)
+}
+
+export const deleteEvent = (eventId) => {
+    return axios.delete(`/api/events/${eventId}`)
+}
+
+export const fetchEvent = (eventId) => {
+    return axios.get(`/api/events/${eventId}`)
+}
+
+export const fetchEvents = () => {
+    return axios.get(`/api/events/allEvents`)
+}

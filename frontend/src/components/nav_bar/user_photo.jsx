b246c7626634808dc4944b3a9de1722e410856bc
hien nguyen <thuhiennguyen198@gmail.com>
2021-12-03 15:06:28 -0500

    "add changes form main'

--- frontend/src/components/nav_bar/user_photo.jsx
+++ frontend/src/components/nav_bar/user_photo.jsx
@@ -1,83 +1,83 @@
-// // import React, { Component } from "react";
-// import axios from "axios";
-// class UserPhoto extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       success: false,
-//       url: "",
-//     };
-//   }
+// import React, { Component } from "react";
+import axios from "axios";
+class UserPhoto extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      success: false,
+      url: "",
+    };
+  }
 
-//   handleChange = (ev) => {
-//     this.setState({ success: false, url: "" });
-//   };
-//   // Perform the upload
-//   handleUpload = (ev) => {
-//     let file = this.uploadInput.files[0];
-//     // Split the filename to get the name and type
-//     let fileParts = this.uploadInput.files[0].name.split(".");
-//     let fileName = fileParts[0];
-//     let fileType = fileParts[1];
-//     console.log("Preparing the upload");
-//     axios
-//       .post("http://localhost:3001/sign_s3", {
-//         fileName: fileName,
-//         fileType: fileType,
-//       })
-//       .then((response) => {
-//         const returnData = response.data.data.returnData;
-//         const signedRequest = returnData.signedRequest;
-//         const url = returnData.url;
-//         this.setState({ url: url });
-//         console.log("Recieved a signed request " + signedRequest);
+  handleChange = (ev) => {
+    this.setState({ success: false, url: "" });
+  };
+  // Perform the upload
+  handleUpload = (ev) => {
+    let file = this.uploadInput.files[0];
+    // Split the filename to get the name and type
+    let fileParts = this.uploadInput.files[0].name.split(".");
+    let fileName = fileParts[0];
+    let fileType = fileParts[1];
+    console.log("Preparing the upload");
+    axios
+      .post("http://localhost:3001/sign_s3", {
+        fileName: fileName,
+        fileType: fileType,
+      })
+      .then((response) => {
+        const returnData = response.data.data.returnData;
+        const signedRequest = returnData.signedRequest;
+        const url = returnData.url;
+        this.setState({ url: url });
+        console.log("Recieved a signed request " + signedRequest);
 
-//         // Put the fileType in the headers for the upload
-//         const options = {
-//           headers: {
-//             "Content-Type": fileType,
-//           },
-//         };
-//         axios
-//           .put(signedRequest, file, options)
-//           .then((result) => {
-//             console.log("Response from s3");
-//             this.setState({ success: true });
-//           })
-//           .catch((error) => {
-//             alert("ERROR " + JSON.stringify(error));
-//           });
-//       })
-//       .catch((error) => {
-//         alert(JSON.stringify(error));
-//       });
-//   };
+        // Put the fileType in the headers for the upload
+        const options = {
+          headers: {
+            "Content-Type": fileType,
+          },
+        };
+        axios
+          .put(signedRequest, file, options)
+          .then((result) => {
+            console.log("Response from s3");
+            this.setState({ success: true });
+          })
+          .catch((error) => {
+            alert("ERROR " + JSON.stringify(error));
+          });
+      })
+      .catch((error) => {
+        alert(JSON.stringify(error));
+      });
+  };
 
-//   render() {
-//     const Success_message = () => (
-//       <div style={{ padding: 50 }}>
-//         <h3 style={{ color: "green" }}>SUCCESSFUL UPLOAD</h3>
-//         <a href={this.state.url}>Access the file here</a>
-//         <br />
-//       </div>
-//     );
-//     return (
-//       <div className="UserPhoto">
-//         <center>
-//           <h1>UPLOAD A FILE</h1>
-//           {this.state.success ? <Success_message /> : null}
-//           <input
-//             onChange={this.handleChange}
-//             ref={(ref) => {
-//               this.uploadInput = ref;
-//             }}
-//             type="file"
-//           />
-//           <br />
-//           <button onClick={this.handleUpload}>UPLOAD</button>
-//         </center>
-//       </div>
-//     );
-//   }
-// }
-// export default UserPhoto;
+  render() {
+    const Success_message = () => (
+      <div style={{ padding: 50 }}>
+        <h3 style={{ color: "green" }}>SUCCESSFUL UPLOAD</h3>
+        <a href={this.state.url}>Access the file here</a>
+        <br />
+      </div>
+    );
+    return (
+      <div className="UserPhoto">
+        <center>
+          <h1>UPLOAD A FILE</h1>
+          {this.state.success ? <Success_message /> : null}
+          <input
+            onChange={this.handleChange}
+            ref={(ref) => {
+              this.uploadInput = ref;
+            }}
+            type="file"
+          />
+          <br />
+          <button onClick={this.handleUpload}>UPLOAD</button>
+        </center>
+      </div>
+    );
+  }
+}
+export default UserPhoto;

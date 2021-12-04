4f3cb7ec1f479f94ac912574dd793027bcbcaca3
hien nguyen <thuhiennguyen198@gmail.com>
2021-12-04 13:46:15 -0500

    remove keys_dev form staged

--- config/keys_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI:
-    'mongodb+srv://dev:5KwfzudwABxeN5MB@dev.shfnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  secretOrKey: '6KqCpOcPnX',
-  //Make sure this is your own unique string
-};
